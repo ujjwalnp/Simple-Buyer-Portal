64a3f033b2ef990e39b54e4709abb18a928de800
ujjwalnp <dhakalujjwal99@gmail.com>
2026-04-08 08:46:56 +0545

    feat: add endpoint to add properties to favourites

--- backend/src/controllers/favourite.controller.js
+++ backend/src/controllers/favourite.controller.js
@@ -1,4 +1,6 @@
+import mongoose from "mongoose";
 import Favourite from "../models/favourite.model.js";
+import Property from "../models/property.model.js";
 
 export const getMyFavourites = async (req, res) => {
     try {
@@ -8,6 +10,31 @@ export const getMyFavourites = async (req, res) => {
         }
         
         res.status(200).json({ favourites });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export const addToFavourites = async (req, res) => {
+    try {
+        const { propertyId } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(propertyId)) {
+            return res.status(400).json({ message: "Invalid Property ID format" });
+        }
+
+        const property = await Property.findById(propertyId);
+        if (!property) {
+            return res.status(404).json({ message: "Property not found" });
+        }
+
+        const existingFavourite = await Favourite.findOne({ user: req.user.id, property: propertyId });
+        if (existingFavourite) {
+            return res.status(409).json({ message: "Property already exists in favourites" });
+        }
+
+        const favourite = await Favourite.create({ user: req.user.id, property: propertyId });
+        res.status(201).json({ favourite });      
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
--- backend/src/routes/favourite.routes.js
+++ backend/src/routes/favourite.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getMyFavourites } from "../controllers/favourite.controller.js";
+import { getMyFavourites, addToFavourites } from "../controllers/favourite.controller.js";
 import { protect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/me", protect, getMyFavourites);
+router.post("/:propertyId", protect, addToFavourites);
 
 export default router;
