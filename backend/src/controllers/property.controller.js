99f7b32388f9840731a45644ff9aa58f9ff930f8
ujjwalnp <dhakalujjwal99@gmail.com>
2026-04-08 08:09:01 +0545

    feat: add properties validation

--- backend/src/controllers/property.controller.js
+++ backend/src/controllers/property.controller.js
@@ -3,6 +3,10 @@ import Property from "../models/property.model.js";
 export const getAllProperties = async (req, res) => {
     try {
         const properties = await Property.find();
+        if (properties.length === 0) {
+            return res.status(404).json({ message: "No properties found" });
+        }
+
         res.status(200).json({ properties });
     } catch (err) {
         res.status(500).json({ message: err.message });
